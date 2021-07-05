26fa80064d6104b394554b7ceaea413a17f57594
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-07-05 18:41:44 -0400

    initial commit

---
+++ js/sap.js
@@ -0,0 +1,10 @@
+// var sapcai = require('sapcai').default
+import sapcai from 'sapcai';
+
+const clientid = "sb-790f46bd-7335-4018-9a8d-3c9ee0a77099-CLONE-RT!b40741|cai-production!b20881";
+const secret = "vFab9NkTiAfaUCNoduDSZGnd6FE";
+const requesttoken = "0ff521cfcb2c4a6e2dfde31ebff0a13a";
+const oauthURL = "https://sapcai-community.authentication.eu10.hana.ondemand.com/oauth/token";
+const requestURL = "https://api.cai.tools.sap/build/v1/dialog";
+
+console.log("here");
